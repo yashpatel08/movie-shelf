3828c031c67015cde819a949ca85317fb484a0e8
Yash <yashpatel54257@gmail.com>
2024-06-01 17:35:52 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -8,18 +8,12 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = process.env.PORT || 4000;
-
 app.use(cors({
-    origin: ["https://movieshelf-phi.vercel.app/"],
-    methods: ["GET", "POST","PUT","DELETE"],
+    origin: ["https://movieshelf-phi.vercel.app", "https://movie-shelf-dun.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
 
-app.use(cors({
-    origin: ["https://movie-shelf-dun.vercel.app"],
-    methods: ["GET", "POST","PUT","DELETE"],
-    credentials: true
-}));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.json());
 app.use(cookieParser());
