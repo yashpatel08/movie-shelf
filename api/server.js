58eaa394696998e6e651da50e70e26fd693a101b
Yash <yashpatel54257@gmail.com>
2024-06-01 22:17:18 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -25,6 +25,8 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(cors());
+
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.json());
 app.use(cookieParser());
