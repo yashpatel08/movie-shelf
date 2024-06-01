0d9045266b39362547a532def13bed970f7db8fe
Yash <yashpatel54257@gmail.com>
2024-06-01 22:05:42 +0530

    c

--- frontend/src/component/Login.js
+++ frontend/src/component/Login.js
@@ -21,7 +21,7 @@ const Login = () => {
     await LoginValidations.validate({ email, password });
 
     try {
-      const response = await axios.post(`movieshelf-fzaj2v3uu-yashpatel08s-projects.vercel.app/users/login`, {
+      const response = await axios.post(`https://movieshelf-two.vercel.app/users/login`, {
         email, password
       });
 
