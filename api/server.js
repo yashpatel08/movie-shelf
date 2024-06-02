114dabcbfc8159a66fdb0cd6253662da9fa0d6c3
Yash <yashpatel54257@gmail.com>
2024-06-02 13:01:44 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -8,11 +8,11 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = process.env.PORT || 4000;
-app.use(cors({
-    origin: "https://movie-shelf-nine.vercel.app",
+app.use(cors({ 
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
-}));
+})); 
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", req.headers.origin);
