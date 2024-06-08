5a7a9bf3fded5e93bb7d9f38fea7ff63dcb67126
Yash <yashpatel54257@gmail.com>
2024-06-08 12:06:23 +0530

    updated

--- api/server.js
+++ api/server.js
@@ -17,7 +17,7 @@ const corsOptions = {
 };
 
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.json());
 app.use(cookieParser());
