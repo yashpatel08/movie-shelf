ae52c54eec64b558a635e229453725d07614cb7c
Yash <yashpatel54257@gmail.com>
2024-06-08 12:35:24 +0530

    Update Register.js

--- frontend/src/component/Register.js
+++ frontend/src/component/Register.js
@@ -22,7 +22,7 @@ const Register = () => {
 
     try {
       if (cpassword === password) {
-        const response = await axios.post(`https://movieshelff.vercel.app/users/register`, {
+        const response = await axios.post(`https://movieshelff.vercel.app/api/users/register`, {
           name,
           email,
           password,
